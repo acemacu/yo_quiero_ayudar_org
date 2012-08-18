506655b84885004fbd425f7fd1d24a2a0e062b28
Ana Martinez <acemacu@gmail.com>
2012-08-17 20:30:26 -0700

    Main project structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YoQuieroAyudarOrg::Application.config.secret_token = '3b050bea43a297d6b29b8bf9546c335b7f87dad9b0db5b3a5e441210e5e167a8971a2d873a6f99d3e0d11d2978861ca833b495cfc59923310a39faa7203cafe6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+YoQuieroAyudarOrg::Application.config.session_store :cookie_store, :key => '_yo_quiero_ayudar_org_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# YoQuieroAyudarOrg::Application.config.session_store :active_record_store
