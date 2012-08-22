f5e3bfb291af81035f9d73fe9e8b77e603f2c54d
Ana Martinez <acemacu@gmail.com>
2012-08-21 23:03:52 -0700

    Added active images when user is in a particular section

--- app/controllers/general_information_controller.rb
+++ app/controllers/general_information_controller.rb
@@ -1,10 +1,12 @@
 class GeneralInformationController < ApplicationController
   
   def how_to_participate
+  	@how_to_participate = true
   	render :template => 'general_information/how_to_participate.html.erb'
   end
 
   def about_us
+  	@about_us = true
   	render :template => 'general_information/about_us.html.erb'
   end
 
