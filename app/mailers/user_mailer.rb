f25c7915e7d42a6bfb8ae1cd0464138f91c0608c
Ana Martinez <acemacu@gmail.com>
2012-10-14 16:22:22 -0700

    Added new email for contact form

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,12 @@
 class UserMailer < ActionMailer::Base
-  default from: "acemacu@gmail.com"
+  default from: "yo.quiero.ayudar.org@gmail.com"
   
   def contact_form(params)
     @user_name    = params[:name] 
     @user_email   = params[:email]
     @message      = params[:message]
     #TODO - Add this as a variable into the admin panel
-    to            = "acemacu@gmail.com"
+    to            = "yo.quiero.ayudar.org@gmail.com"
     mail(to: to, subject: 'Hay un nuevo mensaje de "Quiero Ayudar"')
   end
 end
